51ff2520826eee7e002f0041de3cfd34e892559f
Harshit shukla <harshitshuklaharsh8@gmail.com>
2025-04-13 15:08:03 +0530

    update

---
+++ config/cors.js
@@ -0,0 +1,9 @@
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+module.exports = corsOptions;
